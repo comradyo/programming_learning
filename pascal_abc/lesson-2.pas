30b329ad4a9e03d7223632b4e7f000070cdadebb
Vinnikov Stepan <acesolo@mail.ru>
2025-07-05 14:09:39 +0300

    learning 1

---
+++ pascal_abc/lesson-2.pas
@@ -0,0 +1,9 @@
+program MyProgram; 
+
+var // Объявление переменных
+  x:integer; // название_переменной:тип_переменной
+begin
+  readln(x); // readln = real line = считать линию
+  // Считывает с линии введённую строку
+  writeln('Вы ввели:', x);
+end.
